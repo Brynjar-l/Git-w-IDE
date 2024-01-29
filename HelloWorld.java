e78de7ba88ed973f3c0e324981451321275c5d32
Daniel Andri Styrmisson <danielstyrmisson@gmail.com>
2024-01-29 15:25:43 +0000

    Added a ? sign to the end of the last two lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,7 +9,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HELLO, WORLD!");
-        System.out.println("Halló, Heimur!");
-        System.out.println("Hola, Mundo");
+        System.out.println("Halló, Heimur!?");
+        System.out.println("Hola, Mundo??????????????");
     }
 }
